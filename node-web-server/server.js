c8162d992e280a369cc1793af693ec59c3d50ffe
Cody <stammer.3@osu.edu>
2017-08-06 12:53:54 -0400

    Created basic node server using Express and Handlebars

---
+++ node-web-server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+//console.log(__dirname)
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website!!!!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+
+app.get('/bad', (req, res) => {
+    res.send({errorMessage:'This is not the page you\'re looking for'});
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});

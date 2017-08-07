994d7704691b787debdce92e39eefbecc8e22b2c
Cody <stammer.3@osu.edu>
2017-08-06 22:13:21 -0400

    added maintenance mode functionality

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 //console.log(__dirname)
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+// This is for when site is under maintenance 
+//
+// app.use((req, res, next) =>{
+//     res.render(`maintenance.hbs`, {
+//         pageTitle: `Maintenance Page`,
+//         maintenanceMessage: `This site is currently under maintenance`
+//     });
+// });
+
+app.use((req, res, next) => {
+    //next tells you when middleware function is done
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {if(err){console.log(`unable to append to server.log`)}});
+    next();
+});
+
+//allows calling to files in public folder
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {

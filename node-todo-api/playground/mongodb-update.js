31ba989f11c18ffb68a06f634c031fedef3736f3
Cody <stammer.3@osu.edu>
2017-09-30 11:47:53 -0400

    messed around with update

---
+++ node-todo-api/playground/mongodb-update.js
@@ -0,0 +1,30 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient,  ObjectID} = require('mongodb'); //identical to above
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// // ES6 object destructuring
+// var user = {name: 'cody', age: 24};
+// var {name} = user;
+// console.log(name); // prints cody
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log("Unable to connect to MongoDB server");
+    }
+    console.log("Connected to MongoDB server");
+
+    db.collection('Users').findOneAndUpdate({
+        name: "Casey"
+    }, {
+        $set: {name:"Caseye"},
+        $inc: {age: 1}
+    }, {
+        returnOrignal: false
+    }).then((result) =>{
+        console.log(result);
+    })
+
+    //db.close();
+});
